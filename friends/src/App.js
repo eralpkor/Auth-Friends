146da8c36c6170e9b01ca591dc8f0233e1c4c3ed
eralpkor <eralp.ny@gmail.com>
2019-09-17 19:02:25 -0400

    added Routes to App.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,15 +1,33 @@
-import React from 'react';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 import Login from "./components/Login";
 
-import './App.css';
+import "./App.css";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
-    <div className="App">
-     <Login />
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/protected">Secret Page</Link>
+          </li>
+        </ul>
+        <div className='form-container'>
+          <Switch>
+            <PrivateRoute exact path="/protected">
+              <Route path="/login" component={Login} />
+              <Route component={Login} />
+            </PrivateRoute>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
